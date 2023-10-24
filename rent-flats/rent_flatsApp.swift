f2807a8e542dbe581dffb7e470e376ab6de79f6a
Andrei Avram <andreia@apadmi.com>
2023-10-24 12:51:09 +0300

    Initial Commit

---
+++ rent-flats/rent_flatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rent_flatsApp.swift
+//  rent-flats
+//
+//  Created by Andrei Avram on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct rent_flatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
